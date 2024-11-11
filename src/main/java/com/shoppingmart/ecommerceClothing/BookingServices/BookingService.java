f4701b2c0eed8213a2b139d95a0a7f0370978402
vamshivammi4545 <Vamshivammi45@gmail.com>
2024-11-11 14:48:36 +0530

    same changes

--- src/main/java/com/shoppingmart/ecommerceClothing/BookingServices/BookingService.java
+++ src/main/java/com/shoppingmart/ecommerceClothing/BookingServices/BookingService.java
@@ -7,14 +7,23 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.shoppingmart.ecommerceClothing.productsService.ProductRepo;
+import com.shoppingmart.ecommerceClothing.productsService.Productsdto;
+
 @Service
 public class BookingService {
 
     @Autowired
     private BookingDao bookingDao;
 
+    @Autowired
+    private ProductRepo productRepo;
+
     public BookingDto saveBooking(BookingDto bookingDto)
     {
+        Optional<Productsdto> productsdto=productRepo.findById(bookingDto.getId());
+        bookingDto.setPrice(productsdto.get().getPrice());
+        bookingDto.setTotalPrice(bookingDto.getQuantity()*productsdto.get().getPrice());
         bookingDto.setBookingdateTime(LocalDateTime.now());
         return bookingDao.saveBooking(bookingDto);
         
