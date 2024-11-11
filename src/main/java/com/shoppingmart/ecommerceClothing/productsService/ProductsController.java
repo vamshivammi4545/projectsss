e7e1f4d447da3c2c9ebe4609aada4b08ad0bcb62
Harsha Vardhan Reddy Angala <harshavardhan9932@gmail.com>
2024-11-11 17:46:14 +0530

    retrived all the chaneges

--- src/main/java/com/shoppingmart/ecommerceClothing/productsService/ProductsController.java
+++ src/main/java/com/shoppingmart/ecommerceClothing/productsService/ProductsController.java
@@ -1,11 +1,6 @@
 package com.shoppingmart.ecommerceClothing.productsService;
 
-
-
-import java.util.List;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,15 +12,9 @@ public class ProductsController {
     @Autowired
     private ProductService productService;
 
-    @PostMapping("/Saveproduct")
+    @PostMapping("/saveproduct")
     public String  saveProductsController(@RequestBody Productsdto productsdto){
     return productService.saveProductsService(productsdto);
-}
- @GetMapping("/productId/{i}")
-public Productsdto findProductById(@PathVariable int i) {
-        return productService.findProductById(i);
-    }
-    @GetMapping("/findAllById")
-    public List<Productsdto> findAllById(@RequestBody List<integer> i) {
-        return productService.findAllById(i);
+} 
+
 }
